a4849863d3e961fde05e457b9ae24863d37fb678
Hiền Hoàng <hien.hc@vinapo.com>
2016-02-24 18:51:28 +0700

    added paging recycler adapter

---
+++ app/src/main/java/me/hoangchunghien/popularmovies/ui/common/PagingRecyclerAdapter.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2016.  Chung Hien, Hoang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package me.hoangchunghien.popularmovies.ui.common;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import me.hoangchunghien.popularmovies.R;
+
+public abstract class PagingRecyclerAdapter<T, VH extends RecyclerView.ViewHolder>
+        extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    protected static final int VIEW_TYPE_LOAD_MORE = 1;
+    protected static final int VIEW_TYPE_ITEM = 2;
+
+    @NonNull
+    protected final LayoutInflater mInflater;
+
+    @NonNull
+    protected List<T> mItems;
+
+    protected boolean showLoadMore = false;
+
+    public PagingRecyclerAdapter(@NonNull Context context, @NonNull List<T> items) {
+        mInflater = LayoutInflater.from(context);
+        mItems = items;
+    }
+
+    public void setLoadMore(boolean enabled) {
+        if (showLoadMore != enabled) {
+            if (showLoadMore) {
+                notifyItemRemoved(getItemCount());
+                showLoadMore = false;
+            } else {
+                notifyItemInserted(getItemCount());
+                showLoadMore = true;
+            }
+        }
+    }
+
+    public boolean isLoadMore() {
+        return showLoadMore;
+    }
+
+    public boolean isLoadMore(int position) {
+        return showLoadMore && (position == (getItemCount() - 1));
+    }
+
+    public void setItems(@NonNull List<T> items) {
+        mItems = items;
+        notifyDataSetChanged();
+    }
+
+    int countLoadMore() {
+        return showLoadMore ? 1 : 0;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size() + countLoadMore();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return isLoadMore(position) ? VIEW_TYPE_LOAD_MORE : VIEW_TYPE_ITEM;
+    }
+
+    public void add(@NonNull List<T> newItems) {
+        if (!newItems.isEmpty()) {
+            int currentSize = mItems.size();
+            int amountInserted = newItems.size();
+
+            mItems.addAll(newItems);
+            notifyItemRangeInserted(currentSize, amountInserted);
+        }
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return viewType == VIEW_TYPE_LOAD_MORE
+                ? new RecyclerView.ViewHolder(mInflater.inflate(R.layout.load_more_progress_bar, parent, false)) {}
+                : onCreateItemHolder(parent, viewType);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder.getItemViewType() == VIEW_TYPE_ITEM) {
+            onBindViewHolder(holder, position);
+        }
+    }
+
+    protected abstract void onBindItemHolder(RecyclerView.ViewHolder holder, int position);
+
+    protected abstract VH onCreateItemHolder(ViewGroup parent, int viewType);
+}
